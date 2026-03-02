c36a7a1c5afc9d7cfbdbe8062be9548f15bd366d
Erick Andrade <erick.andrade.ramos1@gmail.com>
2026-03-02 16:48:27 -0600

    first commit

---
+++ src/config/site.ts
@@ -0,0 +1,25 @@
+// import ogImage from "../assets/og-image.png";
+
+export const siteConfig = {
+  name: "PEMSA",
+  nameLarge: "Protección Electrónica Monterrey S.A. de C.V.",
+  description:
+    "Protección Electrónica Monterrey es una empresa mexicana fundada en 1985 que se especializa en el ciclo completo (suministro, instalación, mantenimiento y monitoreo) de sistemas de seguridad electrónica con alcance en todo el país.",
+  url: "https://www.pem-sa.com/",
+  lang: "es",
+  locale: "es_MX",
+  author: "Erick Andrade",
+  twitter: "@Erick-A",
+  // ogImage: ogImage,
+  socialLinks: {
+    twitter: "https://twitter.com",
+    discord: "https://discord.com",
+  },
+  navLinks: [
+    { text: "Inicio", href: "/" },
+    { text: "Nosotros", href: "/about" },
+    { text: "Servicios", href: "/services" },
+    { text: "Blog", href: "/blog" },
+    { text: "Contacto", href: "/contact" },
+  ],
+};
