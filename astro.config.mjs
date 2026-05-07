9e72562fe2a9feaa66fa186820387265727b6dd2
Erick Andrade <erick.andrade.ramos1@gmail.com>
2026-05-07 17:08:34 -0600

    update final

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,21 +1,31 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, envField } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 
 import icon from 'astro-icon';
 
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [icon()],
+  env:{
+    schema:{
+      API: envField.string({context: "client", access: "public" }),
+    }
+  },
+
   prefetch: {
     prefetchAll: true,
     defaultStrategy: "viewport",
   },
+
   build: {
     inlineStylesheets: "always",
   },
+
   vite: {
     plugins: [tailwindcss()]
-  }
+  },
+
 });
